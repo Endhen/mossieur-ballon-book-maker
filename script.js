11613fa8648f16d5021478fb3b658b833a2c608b
Endhen <bogou@gmail.com>
2022-03-17 18:06:32 +0100

    Add 2 pages
    Add introduction type page
    Add transition type page

---
+++ script.js
@@ -0,0 +1,20 @@
+const colors = [
+    '#F2C7C7',  //red
+    '##F2D9EB', //pink
+    '#C3C6E5',  //purple
+    '#BAE3D9',  //green
+    '#E6F2F2',  //blue-green
+]
+
+var test = document.getElementsByClassName('balloon-bottom-left').querySelectorAll('circle');
+
+console.log(test)
+
+
+
+// TODO Page numbers
+// TODO Set colors
+// TODO Set content
+// TODO Set level
+// TODO Set tool number
+
