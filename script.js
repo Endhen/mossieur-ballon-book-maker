331f92ec496b7208e60935f99b316c0799f23d2f
Endhen <bogou@gmail.com>
2022-03-18 16:55:33 +0100

    Add dynamic page numbers Apply multiple instances of random colors for each pages

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ const colors = [
     '#BAE3D9', // green
 ]
 
-var balloonGroups = [
-    document.querySelectorAll('.balloon-bottom-left circle'),
-    document.querySelectorAll('.balloon-top-left circle'),
-    document.querySelectorAll('.balloon-top-right circle'),
-    document.querySelectorAll('.balloon'),
-],  balloonCord = document.querySelectorAll('.balloon-cord');
 
 function rand(min, max) {  
     min = Math.ceil(min);  
@@ -27,26 +21,56 @@ function shuffleArray(arr) {
     return arr
   }
 
-function applyColor(balloons, color) {
 
-    for (let i = 0; i < balloons.length; i++) {
-        balloons[i].style.fill = color
+function setColors(page) {
+
+    var balloonGroups = [
+        document.querySelectorAll('.page:nth-child(' + page +  ') .balloon-bottom-left circle'),
+        document.querySelectorAll('.page:nth-child(' + page +  ') .balloon-top-left circle'),
+        document.querySelectorAll('.page:nth-child(' + page +  ') .balloon-top-right circle'),
+        document.querySelectorAll('.page:nth-child(' + page +  ') .balloon'),
+    ],  balloonCord = document.querySelectorAll('.page:nth-child(' + page +  ') .balloon-cord');
+    
+    function applyColor(balloons, color) {
+    
+        for (let i = 0; i < balloons.length; i++) {
+            balloons[i].style.fill = color
+        }
+        for (let j = 0; j < balloonCord.length; j++) {
+            balloonCord[j].style.stroke = color
+        }
     }
-    for (let j = 0; j < balloonCord.length; j++) {
-        balloonCord[j].style.stroke = color
-    }
+    
+    shuffleArray(colors)
+    
+    balloonGroups.forEach((balloonGroup, i) => {
+        let j =  (i + 1) % colors.length
+        applyColor(balloonGroup, colors[j - 1])  
+    });
 }
 
-shuffleArray(colors)
 
-balloonGroups.forEach((balloonGroup, i) => {
-    let j =  (i + 1) % colors.length
-    console.log(balloonGroup)
-    applyColor(balloonGroup, colors[j - 1])  
-});
+var pages = document.querySelectorAll('.page')
+
 
 // TODO Page numbers
+function setPageNumber(page) {
+    let pageNumber = document.querySelectorAll('.page:nth-child(' + page + ') .page-number')
+    pageNumber[0].innerText = page - 1
+}
+
 // TODO Set content
 // TODO Set level
 // TODO Set tool number
+
+window.onload = function() {
+
+    pages.forEach((page, i) => {
+        page = i + 2
+        setColors(page)
+        setPageNumber(page)
+    })
+}
+
+
 
