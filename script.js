df1a3eb1f018f6b4be3e73e06c57b476e158dca3
Endhen <bogou@gmail.com>
2022-03-18 15:53:59 +0100

    Set random colors accents on graphical decorations

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 const colors = [
-    '#F2C7C7',  //red
-    '##F2D9EB', //pink
-    '#C3C6E5',  //purple
-    '#BAE3D9',  //green
-    '#E6F2F2',  //blue-green
+    '#F2C7C7', // red
+    '#F2D9EB', // pink
+    '#C3C6E5', // purple
+    '#A3D0D9', // blue
+    '#BAE3D9', // green
 ]
 
-var test = document.getElementsByClassName('balloon-bottom-left').querySelectorAll('circle');
+var balloonGroups = [
+    document.querySelectorAll('.balloon-bottom-left circle'),
+    document.querySelectorAll('.balloon-top-left circle'),
+    document.querySelectorAll('.balloon-top-right circle'),
+    document.querySelectorAll('.balloon'),
+],  balloonCord = document.querySelectorAll('.balloon-cord');
 
-console.log(test)
+function rand(min, max) {  
+    min = Math.ceil(min);  
+    max = Math.floor(max);  
+    return Math.floor(Math.random() * (max - min)) + min;   
+}  
 
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr
+  }
 
+function applyColor(balloons, color) {
+
+    for (let i = 0; i < balloons.length; i++) {
+        balloons[i].style.fill = color
+    }
+    for (let j = 0; j < balloonCord.length; j++) {
+        balloonCord[j].style.stroke = color
+    }
+}
+
+shuffleArray(colors)
+
+balloonGroups.forEach((balloonGroup, i) => {
+    let j =  (i + 1) % colors.length
+    console.log(balloonGroup)
+    applyColor(balloonGroup, colors[j - 1])  
+});
 
 // TODO Page numbers
-// TODO Set colors
 // TODO Set content
 // TODO Set level
 // TODO Set tool number
